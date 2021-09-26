066207e4ba86836c356fa53625b53bbf0a5fa67a
Carlo Cuevas <cuevascarlo07@gmail.com>
2021-09-26 17:53:20 +0800

    feat: added background images in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,6 +8,12 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ }) => {
   return (
     <header className={styles.header}>
+      <img alt={'adrenalin'} className={styles.polygon2} src={`/triangle3.svg`} />
+      <img alt={'adrenalin'} className={styles.polygon4} src={`/triangle4.svg`} />
+      <img alt={'adrenalin'} className={styles.polygon5} src={`/triangle5.svg`} />
+      <img alt={'adrenalin'} className={styles.polygon3} src={`/triangle2.svg`} />
+      <img alt={'adrenalin'} className={styles.polygon6} src={`/triangle4.svg`} />
+
       <div className={styles.headerContainer}>
         <img alt={'adrenalin'} className={styles.headerBanner} src={`/icon.svg`} />
         <span className={styles.title}>Mountains</span>
