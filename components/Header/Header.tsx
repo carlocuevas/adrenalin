f912f6119c0674c569c0513daeee941122e9941e
Carlo <cuevascarlo07@gmail.com>
2021-09-27 13:12:19 +0800

    Update Header.tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
+import Image from 'next/image'
 
 type HeaderProps = {
 
@@ -8,14 +9,14 @@ type HeaderProps = {
 const Header: React.FC<HeaderProps> = ({ }) => {
   return (
     <header className={styles.header}>
-      <img alt={'adrenalin'} className={styles.polygon2} src={`/triangle3.svg`} />
-      <img alt={'adrenalin'} className={styles.polygon4} src={`/triangle4.svg`} />
-      <img alt={'adrenalin'} className={styles.polygon5} src={`/triangle5.svg`} />
-      <img alt={'adrenalin'} className={styles.polygon3} src={`/triangle2.svg`} />
-      <img alt={'adrenalin'} className={styles.polygon6} src={`/triangle4.svg`} />
+      <Image alt={'adrenalin'} className={styles.polygon2} src={`/triangle3.svg`} />
+      <Image alt={'adrenalin'} className={styles.polygon4} src={`/triangle4.svg`} />
+      <Image alt={'adrenalin'} className={styles.polygon5} src={`/triangle5.svg`} />
+      <Image alt={'adrenalin'} className={styles.polygon3} src={`/triangle2.svg`} />
+      <Image alt={'adrenalin'} className={styles.polygon6} src={`/triangle4.svg`} />
 
       <div className={styles.headerContainer}>
-        <img alt={'adrenalin'} className={styles.headerBanner} src={`/icon.svg`} />
+        <Image alt={'adrenalin'} className={styles.headerBanner} src={`/icon.svg`} />
         <span className={styles.title}>Mountains</span>
         <p className={styles.subTitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed euismod ex ut blandit condimentum.</p>
       </div>
