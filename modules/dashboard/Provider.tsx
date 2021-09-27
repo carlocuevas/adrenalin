831c3d9a4e199b970de5f430031263e4dd99c596
Carlo <cuevascarlo07@gmail.com>
2021-09-27 09:36:23 +0800

    Update Provider.tsx

--- modules/dashboard/Provider.tsx
+++ modules/dashboard/Provider.tsx
@@ -51,7 +51,7 @@ const AppProvider = ({ children }: AppProviderProps) => {
 
       console.log('mountains', mountains)
 
-      setMountainData(mountains)
+      setMountainData([...mountains])
     }
   }, [sort])
 
